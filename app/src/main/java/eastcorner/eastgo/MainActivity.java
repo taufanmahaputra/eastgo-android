4a61720dd0f84fc6883974415f3b9abeb84465ca
taufanmahaputra <taufanmhptr@gmail.com>
2018-02-24 15:45:49 +0700

    add navigation layout, and init setup on main activity

--- app/src/main/java/eastcorner/eastgo/MainActivity.java
+++ app/src/main/java/eastcorner/eastgo/MainActivity.java
@@ -1,13 +1,57 @@
 package eastcorner.eastgo;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final int DEFAULT_NAVIGATION = 0;
+
+    private AHBottomNavigation mBottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_navigation);
+
+        init();
+    }
+
+    private void init() {
+        //Bottom Setup Navigation Setup
+        createBottomNavigationItems();
+        setDefaultPositionBottomNavigation(DEFAULT_NAVIGATION);
+        setColorAccentBottomNavigation(Color.parseColor(getString(R.color.colorPrimary)));
+        setAlwaysFixedAtBottom(false);
+    }
+
+    private void createBottomNavigationItems() {
+        AHBottomNavigationItem mHome = new AHBottomNavigationItem("Home", R.drawable.ic_home_navigation);
+        AHBottomNavigationItem mOrders = new AHBottomNavigationItem("Orders", R.drawable.ic_orders_navigation);
+        AHBottomNavigationItem mAccount = new AHBottomNavigationItem("My Account", R.drawable.ic_account_navigation);
+
+        mBottomNavigation.addItem(mHome);
+        mBottomNavigation.addItem(mOrders);
+        mBottomNavigation.addItem(mAccount);
+    }
+
+
+    private void setDefaultPositionBottomNavigation(int default_navigation) {
+        mBottomNavigation.setDefaultBackgroundColor(default_navigation);
+    }
+
+    private void setColorAccentBottomNavigation(int color) {
+        mBottomNavigation.setAccentColor(color);
+    }
+
+
+    private void setAlwaysFixedAtBottom(boolean b) {
+        mBottomNavigation.setTranslucentNavigationEnabled(b);
     }
 }
